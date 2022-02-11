95ff4206aa122ff19bc3541590e770f0ff1de3c3
Jordan Sparkes <Jordan.sparkes@jsmsdn.onmicrosoft.com>
2022-02-11 11:06:11 +0000

    working mostly

--- AddressBook/Main Form.Designer.cs
+++ AddressBook/Main Form.Designer.cs
@@ -31,27 +31,35 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Contacts));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.saveToTXTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AddressLine1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AddressLine2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Town = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PostCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelephoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MobileNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DOB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openAddressBookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAddressBookToCSVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem,
-            this.saveToCSVToolStripMenuItem,
-            this.saveToTXTToolStripMenuItem});
+            this.openAddressBookToolStripMenuItem,
+            this.saveAddressBookToCSVToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(500, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1033, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,85 +70,130 @@
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
-            // saveToCSVToolStripMenuItem
-            // 
-            this.saveToCSVToolStripMenuItem.Name = "saveToCSVToolStripMenuItem";
-            this.saveToCSVToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
-            this.saveToCSVToolStripMenuItem.Text = "Save to CSV";
-            // 
-            // saveToTXTToolStripMenuItem
-            // 
-            this.saveToTXTToolStripMenuItem.Name = "saveToTXTToolStripMenuItem";
-            this.saveToTXTToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
-            this.saveToTXTToolStripMenuItem.Text = "Save to TXT";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 39);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(274, 355);
-            this.listBox1.Sorted = true;
-            this.listBox1.TabIndex = 1;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(328, 39);
+            this.label1.Location = new System.Drawing.Point(436, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 33);
             this.label1.TabIndex = 5;
             this.label1.Text = "Contacts";
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(292, 111);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 278);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(44, 122);
+            this.button3.Location = new System.Drawing.Point(550, 469);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(113, 40);
             this.button3.TabIndex = 7;
             this.button3.Text = "Remove Contact";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(44, 194);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 40);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Edit Contact";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(44, 48);
+            this.button1.Location = new System.Drawing.Point(361, 469);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(113, 40);
             this.button1.TabIndex = 5;
             this.button1.Text = "Add Contact";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FirstName,
+            this.Surname,
+            this.AddressLine1,
+            this.AddressLine2,
+            this.Town,
+            this.PostCode,
+            this.TelephoneNumber,
+            this.MobileNumber,
+            this.Email,
+            this.DOB});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1033, 394);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "Forename";
+            this.FirstName.Name = "FirstName";
+            // 
+            // Surname
+            // 
+            this.Surname.HeaderText = "Surname";
+            this.Surname.Name = "Surname";
+            // 
+            // AddressLine1
+            // 
+            this.AddressLine1.HeaderText = "Address Line 1";
+            this.AddressLine1.Name = "AddressLine1";
+            // 
+            // AddressLine2
+            // 
+            this.AddressLine2.HeaderText = "Address Line 2";
+            this.AddressLine2.Name = "AddressLine2";
+            // 
+            // Town
+            // 
+            this.Town.HeaderText = "Town";
+            this.Town.Name = "Town";
+            // 
+            // PostCode
+            // 
+            this.PostCode.HeaderText = "Post Code";
+            this.PostCode.Name = "PostCode";
+            // 
+            // TelephoneNumber
+            // 
+            this.TelephoneNumber.HeaderText = "Telephone Number";
+            this.TelephoneNumber.Name = "TelephoneNumber";
+            // 
+            // MobileNumber
+            // 
+            this.MobileNumber.HeaderText = "Mobile Number";
+            this.MobileNumber.Name = "MobileNumber";
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email Address";
+            this.Email.Name = "Email";
+            // 
+            // DOB
+            // 
+            this.DOB.HeaderText = "Date of Birth";
+            this.DOB.Name = "DOB";
+            // 
+            // openAddressBookToolStripMenuItem
+            // 
+            this.openAddressBookToolStripMenuItem.Name = "openAddressBookToolStripMenuItem";
+            this.openAddressBookToolStripMenuItem.Size = new System.Drawing.Size(123, 20);
+            this.openAddressBookToolStripMenuItem.Text = "Open Address Book";
+            // 
+            // saveAddressBookToCSVToolStripMenuItem
+            // 
+            this.saveAddressBookToCSVToolStripMenuItem.Name = "saveAddressBookToCSVToolStripMenuItem";
+            this.saveAddressBookToCSVToolStripMenuItem.Size = new System.Drawing.Size(156, 20);
+            this.saveAddressBookToCSVToolStripMenuItem.Text = "Save Address Book to CSV";
+            this.saveAddressBookToCSVToolStripMenuItem.Click += new System.EventHandler(this.saveAddressBookToCSVToolStripMenuItem_Click);
             // 
             // Contacts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(500, 401);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(1033, 521);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -148,7 +201,7 @@
             this.Text = "Contacts";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,14 +211,22 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToCSVToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem saveToTXTToolStripMenuItem;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AddressLine1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AddressLine2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Town;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PostCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TelephoneNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MobileNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DOB;
+        private System.Windows.Forms.ToolStripMenuItem openAddressBookToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAddressBookToCSVToolStripMenuItem;
     }
 }
 
--- AddressBook/Main Form.cs
+++ AddressBook/Main Form.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,59 @@ namespace AddressBook
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //int rowIndex dataGridView1.CurrentCell.RowIndex;
+            //dataGridView1.Rows.RemoveAt(rowIndex);
+        }
+
+        private void saveAddressBookToCSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Build the CSV file data as a Comma separated string.
+            string csv = string.Empty;
+
+            //Add the Header row for CSV file.
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                csv += column.HeaderText + ',';
+            }
+
+            //Add new line.
+            csv += "\r\n";
+
+            //Adding the Rows
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    //Add the Data rows.
+                    csv += cell.Value.ToString().Replace(",", ";") + ',';
+                }
+
+                //Add new line.
+                csv += "\r\n";
+            }
+
+            //Exporting to CSV.
+            string folderPath = "C:\\CSV\\";
+            File.WriteAllText(folderPath + "AddressBook.csv", csv);
+        }
+
+        private void saveToCSVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
